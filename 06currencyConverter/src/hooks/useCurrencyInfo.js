bfe99e89459921d0d36816942152f5d5becc2a2c
shikhasinghal1998 <shikha@electrowaveselectronics.com>
2023-12-18 16:53:36 +0530

    Currency converter

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,24 @@
+// Cistom hooks
+import { useEffect, useState } from "react"
+
+// function hello(){
+//     return []
+// }
+
+function useCurrencyInfo(currency){
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    const [data, setData] = useState({})        // Default empty value
+
+    useEffect(() => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+
+        console.log(data)
+    },[currency])
+
+    console.log(data);
+    return data;
+}
+
+export default useCurrencyInfo;
